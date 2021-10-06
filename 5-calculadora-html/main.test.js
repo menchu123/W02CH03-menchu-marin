1802b311ed030f5ffb52f8ca6797e8c724b720c4
Menchu MV <mcmvillalobos@gmail.com>
2021-10-07 00:18:02 +0200

    First failed attemt at testing :(

--- 5-calculadora-html/main.test.js
+++ 5-calculadora-html/main.test.js
@@ -1,1 +1,16 @@
 const operation = require("./main.js");
+
+describe("Given the function operation", () => {
+  describe("When it recieves a '9' and an '8' and the operator is '-'", () => {
+    test("Then it should return '1", () => {
+      const data1 = "9";
+      const data2 = "8";
+      const operator = "-";
+      const expected = "1";
+
+      const result = operation(data1, data2);
+
+      expect(result).toBe(expected);
+    });
+  });
+});
